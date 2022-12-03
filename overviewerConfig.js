19a38114edfb198824ed7beb9459df59db386cc1
Doresun <80627854+Doresun@users.noreply.github.com>
2022-12-03 16:14:39 +0300

    map

---
+++ overviewerConfig.js
@@ -0,0 +1,72 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "markers/marker_home.png",
+            "spawnMarker2x": "markers/marker_home_2x.png",
+            "queryMarker": "markers/marker_location.png",
+            "queryMarker2x": "markers/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "3": "compass_lower-left.png",
+                "2": "compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "2chunks"
+    ],
+    "map": {
+        "debug": false,
+        "cacheTag": "1670072646",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        }
+    },
+    "tilesets": [
+        {
+            "name": "2 \u0447\u0430\u043d\u043a\u0430 \u0432 \u0434\u0438\u0430\u043c\u0435\u0442\u0440\u0435",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "2chunks",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "2chunks",
+            "last_rendertime": 1670070409,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                0,
+                80,
+                0
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": [
+                0,
+                80,
+                0
+            ],
+            "north_direction": 0
+        }
+    ]
+};
